02f3c35aafdfb33ff1520346ca0d22b5a92afb8e
Paul Gent <Paul@GentMail.com>
2021-08-19 20:05:30 +0100

    Foxy now moves

--- src/anim.c
+++ src/anim.c
@@ -17,10 +17,10 @@ void endAnim() {
 void drawAnimFrame(Anim *anim, Vector2 position, float deltaTime){
 
     Rectangle frame = anim->frame;
-    frame.x = frame.x + ((frame.width) * anim->currentFrame);
+    frame.x = frame.x + (frame.width * anim->currentFrame);
     DrawTexturePro(spriteAtlas, frame, (Rectangle){position.x, position.y, anim->frame.width*2, anim->frame.height*2}, (Vector2){0,0}, 0, WHITE);
 
-    anim->time += (deltaTime);
+    anim->time += deltaTime;
 
     if(anim->time >= anim->delay){
         anim->currentFrame ++;
